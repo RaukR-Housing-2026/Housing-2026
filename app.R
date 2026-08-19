b35d8d68dec8fd7631911c547eabaf475f139b9f
Dries Boers <>
2026-08-19 13:23:49 +0200

    Add disk; react when correct click

--- app.R
+++ app.R
@@ -1,12 +1,31 @@
 library(shiny)
-library(leaflet)
 
 source("plot_data.R")
 
-ui <- plotOutput("map")
+ui <- plotOutput("map", click = "map_click")
 
 server <- function(input, output) {
-   output$map <- renderPlot(
-      map) }
-
+   
+   disk <-
+   st_sample(
+      st_buffer(water, -25000), # Button only fully in water
+      size = 1) %>%
+      st_buffer(dist = 25000) # Button size
+      
+      
+   hit <- reactiveVal(FALSE)
+   observeEvent(input$map_click, {
+      req(input$map_click)
+      click_pt <-
+      st_point(c(input$map_click$x, input$map_click$y)) %>%
+      st_sfc(crs = st_crs(disk) )
+         
+      if ( any( st_intersects(click_pt, disk, sparse = FALSE ) ) ) { hit(TRUE) } } )
+         
+   output$map <-
+      renderPlot(
+         if( !hit() ) { map + geom_sf(data = disk)
+         } else       {  map + geom_sf(data = disk) + ggtitle('Got it!') }
+         ) }
+         
 shinyApp(ui=ui, server=server)
